72b58fa9932d1fdee272bfde9ef4bf11363e170c
Sakalya11 <sakalya.m11@gmail.com>
2024-07-23 15:27:05 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a and b"<<endl;
+	cin>>a;
+	cin>>b;
+	int sum1=a+b;
+	cout<<sum1<<endl;
+	return 0;
+}
