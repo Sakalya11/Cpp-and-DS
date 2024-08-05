1be084a0cc2521922dd244346c61e84e99c3b3bc
Sakalya11 <sakalya.m11@gmail.com>
2024-08-05 11:23:24 +0530

    Add files via upload

---
+++ EXP5.1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+// Sakalya Tatte PRN: 23070123142
+int main() 
+{
+    // if statements 
+    int x = 20;
+    int y = 18;
+    if (x > y)
+    {
+        cout<< "x is greater than y "<<endl;
+    }
+    
+
+    // else statement
+    int time = 20;
+    if (time < 18) 
+    {
+        cout<<"Good Morning"<<endl;
+    }
+    else{
+        cout<<"Good Evening"<<endl;
+    }
+
+    // simple if else 
+
+    int  num;
+    cout<<"Enter positive number: ";
+    cin>>num;
+    if(num==0) {
+        cout<<num<<"You have entered zero"<<endl;
+    }
+    else if(num%2!=0){
+        cout<<num<<" is odd"<<endl;
+    }
+    else {
+        cout<<num<<" is even"<<endl;
+    }
+    
+    return 0;
+
+
+}
---
+++ EXP5.2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+//Name: Sakalya Tatte PRN:23070123142
+int main() {
+    int  num1, num2, num3;
+    cout<<"Enter number-1: ";
+    cin>>num1;
+    cout<<"Enter number-2: ";
+    cin>>num2;
+    cout<<"Enter number-3: ";
+    cin>>num3;
+
+    if(num1>num2) {
+        if(num1>num3) {
+            cout<<num1<<" is the greatest number"<<endl;
+        }
+        else {
+            cout<<num3<<" is the greatest number"<<endl;
+        }
+    }
+    else if(num2>num1) {
+        if(num2>num3) {
+            cout<<num2<<" is the greatest number"<<endl;
+        }
+        else {
+            cout<<num3<<" is the greatest number"<<endl;
+        }
+    }
+    else {
+        cout<<"All three numbers are equal";
+    return 0;
+    }
+}
